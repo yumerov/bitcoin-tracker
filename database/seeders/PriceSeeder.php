b9d733ae47d48b8b114c0b566cd2c0cab2e0384f
yumerov <yumerov.levent@gmail.com>
2023-02-26 18:42:54 +0200

    Added price seeder

---
+++ database/seeders/PriceSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Price;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use \Faker\Factory as Faker;
+
+class PriceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+
+        $price = $faker->randomFloat(2, 10000, 30000);
+        $timestamp = Carbon::now();
+
+        for ($i = 1; $i <= 100; $i++) {
+            $price = $price + $faker->randomFloat(2, -100, 100);
+            $product = new Price([
+                'price' => $price,
+                'timestamp' => $timestamp->addMinute()->format('Y-m-d H:i:s')
+            ]);
+            $product->save();
+        }
+    }
+}
