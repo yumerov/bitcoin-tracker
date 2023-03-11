3003968c705b599f7718ea47ee1e28e2920616be
yumerov <yumerov.levent@gmail.com>
2023-03-11 23:50:49 +0200

    PriceNotificationRequest

---
+++ app/Http/Requests/PriceNotificationRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\PriceNotification;
+use App\Rules\UniquePriceNotification;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use \Illuminate\Contracts\Validation\Validator;
+use Symfony\Component\HttpFoundation\Response;
+
+class PriceNotificationRequest extends FormRequest
+{
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email',
+            'price' => [
+                'required',
+                'numeric',
+                'gt:0',
+                new UniquePriceNotification($this->get('email'), $this->get('price'))
+            ]
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'errors' => $validator->errors(),
+        ], Response::HTTP_UNPROCESSABLE_ENTITY));
+    }
+}
