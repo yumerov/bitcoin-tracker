ef34e19f3bfe1d692d19c2da3eb34c46d8594663
yumerov <yumerov.levent@gmail.com>
2023-03-13 02:41:47 +0200

    Email config added

--- config/logging.php
+++ config/logging.php
@@ -117,6 +117,12 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
+
+        'email' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/email.log'),
+            'level' => 'debug',
+        ],
     ],
 
 ];
